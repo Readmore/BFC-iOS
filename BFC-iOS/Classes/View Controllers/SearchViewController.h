7e8b86314144cf91f450c9dc25f4144a9c32b14f
Matt Galloway <matt@architactile.com>
2011-10-07 21:01:40 -0500

    Created About, Search and You Are Here view controllers and views

---
+++ BFC-iOS/Classes/View Controllers/SearchViewController.h
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.h
+//  BFC-iOS
+//
+//  Created by Matt Galloway on 10/7/11.
+//  Copyright 2011 Architactile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SearchViewController : UIViewController {
+    
+}
+
+@end
---
+++ BFC-iOS/Classes/View Controllers/YouAreHereViewController.h
@@ -0,0 +1,16 @@
+//
+//  YouAreHereViewController.h
+//  BFC-iOS
+//
+//  Created by Matt Galloway on 10/7/11.
+//  Copyright 2011 Architactile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface YouAreHereViewController : UIViewController {
+    
+}
+
+@end
