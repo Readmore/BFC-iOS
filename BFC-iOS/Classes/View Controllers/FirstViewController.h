8378951e46a19a31bb5ad832f9a538481096e58c
Matt Galloway <matt@architactile.com>
2011-10-07 20:47:47 -0500

    Reorganize directory structure

---
+++ BFC-iOS/Classes/View Controllers/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  BFC-iOS
+//
+//  Created by Brandon Pollet on 10/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
